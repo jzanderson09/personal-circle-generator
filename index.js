bc0f8629c87626a2ad2a96d4785e9fd372aa8331
jzanderson09 <91779418+jzanderson09@users.noreply.github.com>
2024-04-11 18:32:08 -0500

    Add Style
    Center content, add container, minimal style and click sound.

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-const btn = document.querySelector("button");
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+const btn = document.querySelector('button');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   canvas.width = document.documentElement.clientWidth;
   canvas.height = document.documentElement.clientHeight;
 });
-
-function random(number) {
-  return Math.floor(Math.random() * number);
-}
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (let i = 0; i < 100; i++) {
     ctx.beginPath();
-    ctx.fillStyle = "rgb(255 0 0 / 50%)";
+    ctx.fillStyle = 'rgb(255 0 0 / 50%)';
     ctx.arc(
       random(canvas.width),
       random(canvas.height),
@@ -26,5 +22,12 @@ function draw() {
     ctx.fill();
   }
 }
+function random(number) {
+  return Math.floor(Math.random() * number);
+}
 
-btn.addEventListener("click", draw);
+function playAudio(url) {
+  new Audio(url).play();
+}
+
+btn.addEventListener('click', draw);
